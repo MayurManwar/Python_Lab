a1d08014887f8d1d3088777b489b6edb24c0bc5a
Mayur Manwar <97542949+MayurManwar@users.noreply.github.com>
2024-07-15 15:59:56 +0530

    Add files via upload

---
+++ Assignment 20.py
@@ -0,0 +1,25 @@
+
+#Write a Python program to get the key, value and item in a dictionary.
+# Input dictionary
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# Print header
+print("key", "value")
+
+# Iterate through the dictionary and print key, value pairs
+for key, value in dict_num.items():
+    print(key, value)
+
+
+
+"""
+= RESTART: C:/Users/Mayur Manwar/Desktop/Python code/Githup_python/Assignment 20.py
+key value
+1 10
+2 20
+3 30
+4 40
+5 50
+6 60
+
+"""
